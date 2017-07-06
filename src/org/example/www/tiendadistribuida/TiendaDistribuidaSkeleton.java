0b252faa3381960b1ad0e6cae86bb1e17ba3a14b
vladern <vladys.3234@gmail.com>
2017-07-06 16:47:30 +0200

    ultimos cambios

--- src/org/example/www/tiendadistribuida/TiendaDistribuidaSkeleton.java
+++ src/org/example/www/tiendadistribuida/TiendaDistribuidaSkeleton.java
@@ -5,28 +5,23 @@
  * This file was auto-generated from WSDL
  * by the Apache Axis2 version: 1.6.3  Built on : Jun 27, 2015 (11:17:49 BST)
  */
-    package org.example.www.tiendadistribuida;
-    /**
-     *  TiendaDistribuidaSkeleton java skeleton for the axisService
-     */
-    public class TiendaDistribuidaSkeleton{
-        
-         
-        /**
-         * Auto generated method signature
-         * 
-                                     * @param validar 
-             * @return validarResponse 
-         */
-        
-                 public org.example.www.tiendadistribuida.ValidarResponse validar
-                  (
-                  org.example.www.tiendadistribuida.Validar validar
-                  )
-            {
-                //TODO : fill this with the necessary business logic
-                throw new  java.lang.UnsupportedOperationException("Please implement " + this.getClass().getName() + "#validar");
-        }
-     
-    }
-    
+package org.example.www.tiendadistribuida;
+
+/**
+ * TiendaDistribuidaSkeleton java skeleton for the axisService
+ */
+public class TiendaDistribuidaSkeleton {
+
+	/**
+	 * Auto generated method signature
+	 * 
+	 * @param validar
+	 * @return validarResponse
+	 */
+
+	public org.example.www.tiendadistribuida.ValidarResponse validar(org.example.www.tiendadistribuida.Validar validar) {
+		// TODO : fill this with the necessary business logic
+		throw new java.lang.UnsupportedOperationException("Please implement " + this.getClass().getName() + "#validar");
+	}
+
+}
