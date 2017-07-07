e0543717342ff42a3fd55bf59e4cf401fd87a5d1
vladern <vladys.3234@gmail.com>
2017-07-07 15:46:06 +0200

    implementado el metodo de validar tienda

--- src/org/example/www/tiendadistribuida/TiendaDistribuidaSkeleton.java
+++ src/org/example/www/tiendadistribuida/TiendaDistribuidaSkeleton.java
@@ -1,27 +1,43 @@
-
-/**
- * TiendaDistribuidaSkeleton.java
- *
- * This file was auto-generated from WSDL
- * by the Apache Axis2 version: 1.6.3  Built on : Jun 27, 2015 (11:17:49 BST)
- */
 package org.example.www.tiendadistribuida;
 
-/**
- * TiendaDistribuidaSkeleton java skeleton for the axisService
- */
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+
 public class TiendaDistribuidaSkeleton {
 
-	/**
-	 * Auto generated method signature
-	 * 
-	 * @param validar
-	 * @return validarResponse
-	 */
+	private String URI = "http://apirestmtis-2017.herokuapp.com/api/tienda/";
+	
+	private boolean validarTienda(String string) 
+	{
+		// Crea un cliente para consumir servicios REST
+		Client cliente = ClientBuilder.newClient();
 
-	public org.example.www.tiendadistribuida.ValidarResponse validar(org.example.www.tiendadistribuida.Validar validar) {
-		// TODO : fill this with the necessary business logic
-		throw new java.lang.UnsupportedOperationException("Please implement " + this.getClass().getName() + "#validar");
+		// Crea un objeto WebTarget para acceder al servicio
+		// indicado en la URI
+		WebTarget servicio = cliente.target(URI + string);
+
+		try {
+			// Realiza la petición GET, obteniendo el cuerpo de la respuesta
+			// como un objeto de tipo String
+			String texto = servicio.request().get(String.class);
+
+			// saca por pantalla el contenido de la respuesta
+			System.out.println(texto);
+			return true;
+		} catch (Exception e) {
+			// Ha ocurrido algún error
+			System.out.println("No se ha encontrado el recurso.");
+			return false;
+		}
+	}
+	public org.example.www.tiendadistribuida.ValidarResponse validar(org.example.www.tiendadistribuida.Validar validar) 
+	{
+		org.example.www.tiendadistribuida.ValidarResponse res = new org.example.www.tiendadistribuida.ValidarResponse();
+		String cif = validar.getCif();
+		boolean respuesta = validarTienda(cif);
+		res.setValidada(respuesta);
+		return res;
 	}
 
 }
